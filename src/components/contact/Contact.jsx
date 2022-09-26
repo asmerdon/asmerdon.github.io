b7811ffbcf6eea8eced7977fd82a9ad99873d6f4
asmerdon <alexsmerdon7@gmail.com>
2022-09-26 14:05:47 +0100

    New Components
    Added portfolio section.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './contact.css'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_3jo3m7h', 'template_0ip5at6', form.current, 'EUb2aHvjj9RIa9F8j')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
+
   return (
-    <section id = 'contact'>Contact</section>
+    <section id = 'contact'>
+      <h2>Contact Me</h2>
+      <div>
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" name='name' placeholder='Your Full Name' required/>
+        <input type="email" name='email' placeholder='Your Email' requiired/>
+        <textarea name="message" rows="7" placeholder='Your Message' required/>
+        <button type='submit' className='btn btn-primary'>Send Message</button>
+      </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image"><img src={IMG1} alt=""></img></div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href='https://github.com/' className='btn' target='_blank'>GitHub</a>
+            <a href='https://github.com/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image"><img src={IMG1} alt=""></img></div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href='https://github.com/' className='btn' target='_blank'>GitHub</a>
+            <a href='https://github.com/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
