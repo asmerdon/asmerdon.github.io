b050cc9942e055e7d06731f4c8f99205c2490b3a
asmerdon <alexsmerdon7@gmail.com>
2022-09-26 16:03:32 +0100

    All components finished.
    All components finished, styling not yet done.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FaKaggle} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'></a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Experience</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+      <a href="https://www.linkedin.com/in/alex-smerdon/" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/asmerdon/" target="_blank"><FaGithub /></a>
+        <a href="https://www.kaggle.com/alexandersmerdon" target="_blank"><FaKaggle /></a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Alexander Smerdon, 2022. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
